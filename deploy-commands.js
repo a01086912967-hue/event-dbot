16508023df2b61f07d11a31f59fa2b40c5ff4730
soddu <a01086912967@gmail.com>
2026-05-31 01:29:45 +0900

    first upload

---
+++ deploy-commands.js
@@ -0,0 +1,48 @@
+const { REST, Routes, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const token = process.env.TOKEN;
+const commands = [
+    new SlashCommandBuilder()
+        .setName('룰렛')
+        .setDescription('이벤트를 생성합니다.')
+        .addStringOption(option =>
+            option.setName('시간')
+                .setDescription('예: 1시간')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('인원')
+                .setDescription('당첨 인원')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('상품이름')
+                .setDescription('상품명')
+                .setRequired(true))
+        .addRoleOption(option =>
+            option.setName('보유역할')
+                .setDescription('참가에 필요한 역할')
+                .setRequired(true))
+        .addUserOption(option =>
+            option.setName('뽑기')
+                .setDescription('뽑기를 진행합니다. (관리자 전용)')
+                .setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('슬래시 명령어 등록 중...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                '1510195820073455646',
+                '1456729030459134115'
+            ),
+            { body: commands },
+        );
+
+        console.log('등록 완료!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
---
+++ index.js
@@ -0,0 +1,212 @@
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Events
+} = require('discord.js');
+
+const ms = require('ms');
+
+const TOKEN = process.env.TOKEN;
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+const events = new Map();
+
+client.once(Events.ClientReady, () => {
+    console.log(`${client.user.tag} 온라인!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+
+    if (interaction.isChatInputCommand()) {
+
+        if (interaction.commandName === '룰렛') {
+
+            const time = interaction.options.getString('시간');
+            const winnersCount = interaction.options.getInteger('인원');
+            const prize = interaction.options.getString('상품이름');
+            const role = interaction.options.getRole('보유역할');
+            const forcedWinner = interaction.options.getUser('뽑기');
+
+            const eventId = Date.now().toString();
+
+            events.set(eventId, {
+    participants: [],
+    winnersCount,
+    prize,
+    roleId: role.id,
+    hostId: interaction.user.id,
+    forcedWinnerId: forcedWinner?.id || null,
+    endTime: Date.now() + ms(time)
+});
+
+            const embed = new EmbedBuilder()
+    .setColor('Green')
+    .setTitle(prize)
+    .setDescription(
+`호스트: <@${interaction.user.id}>
+
+참가자: 0명
+
+**당첨자: 추첨 전**
+
+### 보유 역할
+${role}`
+);
+
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`join_${eventId}`)
+                        .setLabel('🎉 참가하기')
+                        .setStyle(ButtonStyle.Success)
+                );
+
+            await interaction.reply({
+    content: '✨ 이벤트 생성 완료!',
+    ephemeral: true
+});
+
+const message = await interaction.channel.send({
+    content: '@everyone',
+    embeds: [embed],
+    components: [row]
+});
+
+            setTimeout(async () => {
+                const data = events.get(eventId);
+
+                if (!data) return;
+
+                const participants = data.participants;
+
+if (participants.length === 0) {
+                    interaction.channel.send('🎉 이벤트 종료!\n참가자가 없습니다.');
+                    events.delete(eventId);
+                    return;
+                }
+
+                let winners;
+
+if (data.forcedWinnerId) {
+    winners = [data.forcedWinnerId];
+} else {
+    const shuffled = [...participants]
+        .sort(() => Math.random() - 0.5);
+
+    winners = shuffled.slice(
+        0,
+        Math.min(data.winnersCount, participants.length)
+    );
+}
+                let winnerList = '';
+                const endedEmbed = EmbedBuilder.from(message.embeds[0])
+.setColor('Red')
+.setDescription(`
+⛔ <t:${Math.floor(data.endTime / 1000)}:R> 종료됨
+
+호스트: <@${data.hostId}>
+
+참가자: ${participants.length}명
+
+**당첨자: ${winners.map(id => `<@${id}>`).join(', ')}**
+`);
+
+await message.edit({
+    embeds: [endedEmbed],
+    components: []
+});
+
+                winners.forEach((id, index) => {
+                    winnerList += `${index + 1}. <@${id}>\n`;
+                });
+
+                interaction.channel.send(`
+
+-# ${winners.map(id => `<@${id}>`).join(', ')} 님, 당첨 축하드려요!
+
+상품: ${data.prize}
+
+당첨자
+
+${winnerList}
+`);
+
+                events.delete(eventId);
+
+            }, ms(time));
+        }
+    }
+
+    if (interaction.isButton()) {
+
+        if (!interaction.customId.startsWith('join_')) return;
+
+        const eventId = interaction.customId.split('_')[1];
+
+        const data = events.get(eventId);
+
+        if (!data) {
+            return interaction.reply({
+                content: '이미 종료된 이벤트입니다.',
+                ephemeral: true
+            });
+        }
+
+        const member = await interaction.guild.members.fetch(
+            interaction.user.id
+        );
+
+        if (!member.roles.cache.has(data.roleId)) {
+            return interaction.reply({
+                content: '역할이 없어, 이벤트 참가가 불가능합니다! ❌',
+                ephemeral: true
+            });
+        }
+
+        if (data.participants.includes(interaction.user.id)) {
+            return interaction.reply({
+                content: '이미 참가했습니다!',
+                ephemeral: true
+            });
+        }
+
+        data.participants.push(interaction.user.id);
+
+const oldEmbed = interaction.message.embeds[0];
+
+const newEmbed = EmbedBuilder.from(oldEmbed)
+.setDescription(`
+${Date.now() < data.endTime ? '🟢' : '⛔'} <t:${Math.floor(data.endTime / 1000)}:R> 종료됨
+
+호스트: <@${data.hostId}>
+
+참가자: ${data.participants.length}명
+
+**당첨자: 추첨 전**
+
+### 보유 역할
+<@&${data.roleId}>
+`)
+
+await interaction.message.edit({
+    embeds: [newEmbed]
+});
+
+interaction.reply({
+    content: '🎉 참가 완료!',
+    ephemeral: true
+});
+    }
+});
+
+client.login(TOKEN);
